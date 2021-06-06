2fc6fbeee72b2b57b99be19be711c6660ef4f950
2020147569 <pom0319@gmail.com>
2021-06-06 23:38:15 +0900

    bug fix 8

--- server.js
+++ server.js
@@ -62,23 +62,7 @@ var conf = {
 // use origin undefined handler, then cors
 app.use(conf.originUndefined, cors(conf.cors));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
-
-app.get('/', async(req, res) => {
+app.post('/', async(req, res) => {
     /*let category = req.body.category;
     let people = req.body.pnum;
     let time = req.body.time;
