139758efa208a74d07c39266c0a4a1050fe927c1
2020147569 <pom0319@gmail.com>
2021-06-07 01:19:45 +0900

    bug fix 10

--- myserver.js
+++ myserver.js
@@ -7,7 +7,23 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async(req, res) => {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+
+app.post('/', async(req, res) => {
     /*let category = req.body.category;
     let people = req.body.pnum;
     let time = req.body.time;
