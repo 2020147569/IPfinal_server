88cbca7127e41cbd6f76c95b1bf3515e3d980d58
2020147569 <pom0319@gmail.com>
2021-06-07 23:24:11 +0900

    server test 5

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-    res.send(req.body);
+    res.send(req.latitude);
 })
 app.get('/', async(req, res) => {
     /*let category = req.body.category;
